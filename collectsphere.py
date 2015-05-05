d55ad73f3af4f53dbe797c48b996b207e6631fd2
Mathias Ewald <mewald@team.mobile.de>
2015-05-05 15:49:00 +0200

    added comments

--- collectsphere.py
+++ collectsphere.py
@@ -20,8 +20,8 @@ import ssl
 import re
 from pysphere import VIServer
 
-CONFIGS = []
-ENVIRONMENT = {}
+CONFIGS = []            # Stores the configuration as passed from collectd
+ENVIRONMENT = {}        # Runtime data and object cache
 
 #####################################################################################
 # IMPLEMENTATION OF COLLECTD CALLBACK FUNCTIONS 
@@ -31,15 +31,16 @@ def configure_callback(conf):
     """Receive configuration block. This is called by collectd for every
     configuration block it finds for this module."""
 
+    # Set some sensible default values
     name = None
     host = None
-    port = None
+    port = 443 
     verbose = None
-    username = None
-    password = None
-    host_counters = []
-    vm_counters = []
-    inventory_refresh_interval = None
+    username = 'root'                   
+    password = 'vmware'                 
+    host_counters = ['cpu.usage', 'mem.usage']
+    vm_counters = ['cpu.usage','mem.usage']
+    inventory_refresh_interval = 600  
 
     for node in conf.children:
         key = node.key.lower()
@@ -81,6 +82,8 @@ def init_callback():
     Server. This includes creating the connection, reading in counter ID
     mapping tables and spawning inventory watch dogs. """
 
+    # For every set of configuration received from collectd, a environment must
+    # be created. Further, a inventory watch dog thread has to be spawned.
     for config in CONFIGS:
         env = create_environment(config)
       
@@ -91,15 +94,25 @@ def init_callback():
         env['inventory'] = inventory
         env['watchdog'] = thread
 
+        # The environment is stored under the name of the config block
         ENVIRONMENT[config.get("name")] = env
  
 def read_callback():
+    """ This function is regularly executed by collectd. It is important to
+    minimize the execution time of the function which is why a lot of caching
+    is performed using the environment objects. """
+
+    # We are going to spawn a lot of threads soon to speed up metric fetching. References to the threads are stored here.
     threads = []
+    
+    # Walk through the existing environments
     for name in ENVIRONMENT.keys():
         env = ENVIRONMENT[name]
         collectd.info("Entering environment: " + name)
         conn = env['conn']
 
+        # Reconnect to vCenter Server if necessary and give up if a connection
+        # cannot be established.
         if(conn.is_connected() == False):
             collectd.info("Reconnecting")
             reconnect(name)
@@ -107,35 +120,48 @@ def read_callback():
             if(conn.is_connected() == False):
                 collectd.info("Reconnect unsuccessful. Giving up.")
                 continue
-
+        
+        # fetch the instance of perf manager from the object cache
         pm = env['pm']
 
-        # HOST METRICS     
+        # Host Metrics: Walk the _host_ inventory tree created by the inventory
+        # watch dog and spawn a thread for every host.
+        
         host_counter_ids = env.get('host_counter_ids')
-        inventory = env.get('inventory')
-        host_inventory = inventory.get('host_inventory')
+        host_inventory = env.get('inventory').get('host_inventory')
 
+        # Only do this if the inventory tree is not empty. If is isn't: for
+        # every host in every cluster spawn a thread.
         if host_inventory:
             for cluster_name in host_inventory.keys():
                 for host_name in host_inventory.get(cluster_name).get('hosts').keys():
+                    
+                    # fetch the host MOR from the inventory tree
                     host = host_inventory.get(cluster_name).get('hosts').get(host_name)
+                   
+                    # create thread and execute
                     thread = GetMetricsThread(pm, host, host_counter_ids, name, cluster_name, host_name.split('.')[0])
-                    threads.append(thread)
                     thread.start()
+        
+                    # append the thread to the list of threads
+                    threads.append(thread)
         
         collectd.info("Spawned " + str(len(threads)) + " threads to fetch metrics.")
  
+    # Wait for all threads to finish. Then dispatch all gathered values to collectd.
     for th in threads:
         th.join()
 
+        # The thread was spawned with some information that we now need to
+        # properly dispatch the values. Fetch that information now.
         vc_name = th.vc_name
         cluster_name = th.cluster_name
         host_name = th.host_name
         stats = th.stats
 
-        env = ENVIRONMENT.get(vc_name)
-
+        # For every stat object work up the data, then dispatch
         for stat in stats:
+
             counter = stat.counter
             group = stat.group
             instance = stat.instance
@@ -144,9 +170,19 @@ def read_callback():
             value = float(stat.value)
             timestamp = time.mktime(dtime.timetuple())
 
+            # When the instance value is empty, the vSphere API references a
+            # total. Example: A host has multiple cores for each of which we
+            # get a single stat object. An additional stat object will be
+            # returned by the vSphere API with an empty string for "instance".
+            # This is the overall value accross all logical CPUs.
             if(len(stat.instance.strip()) == 0):
                 instance = 'average'
-            
+           
+            # We are limited to 63 characters for the type_instance field. This
+            # is why we need to shorten NAA canonical names and VMFS UUIDs. For
+            # NAAs it makes the most sense to use the last 6 characters while
+            # UUIDs should be unique with the first 6. Unfortunately, there is
+            # a chance to collisions for VMFS UUIDs.
             if 'naa' in instance:
                 values = instance.split('.')
                 instance = values[0] + values[1][-6:]
@@ -154,6 +190,9 @@ def read_callback():
             if re.search('^[0-9a-f-]+$', instance):
                 instance = instance[:6] 
         
+            # Now we used the collectd API for dispatch the information to
+            # collectd which will then take care of sending it to rrdtool,
+            # graphite or whereever.
             cd_value = collectd.Values(plugin="collectsphere")
             cd_value.type = "gauge"
             cd_value.type_instance = cluster_name + "." + host_name + "." + group + "." + instance + "." + counter + "." + unit
@@ -164,7 +203,8 @@ def read_callback():
 
 def shutdown_callback():
 	""" Called by collectd on shutdown. """
-	
+
+    # Disconnect all existing vCenter connections
 	for vc_name in ENVIRONMENT.keys():
 		env = ENVIRONMENT.get(vc_name)
 		conn = env.get('conn')
@@ -176,38 +216,111 @@ def shutdown_callback():
 #####################################################################################
 
 def create_environment(config):
-    """ Creates environment in ENVIRONMENT from the passed configuration. """
+    """
+    Creates a runtime environment from a given configuration block. As the
+    structure of an environment is a bit complicates, this is the time to
+    document it:
+   
+    A single environment is a dictionary that stores runtime information about
+    the connection, metrics, etc for a single vCenter Server. This is the
+    structure pattern:
+
+        {
+            'conn': <INSTANCE OF VIServer FROM THE PYSPHERE API>,
+            'pm': <INSTANCE OF THE PERFORMANCE MANAGER>,
+                
+            # This is a dictionary that stores mappings of performance counter
+            # names to their respective IDs in vCenter.
+            'lookup_host': {    
+                'NAME': <ID>,       # Example: 'cpu.usage': 2
+                ...
+            },
+
+            # The same lookup dictionary must be available for virtual machines:
+            'lookup_vm': {
+                'NAME': <ID>,
+                ...
+            },
+            
+            # This stores the IDs of the counter names passed via the
+            # configuration block. We used the lookup tables above to fill in
+            # the IDs.
+            'host_counter_ids': [<ID>, <ID>, ...],
+            'vm_counter_ids': [<ID>, <ID>, ...],
+
+            # The inventory watch dog reponsible for this vCenter Server fills
+            # in the contents here. We store a host_inventory and a
+            # vm_inventory separately. The host_inventory contains a tree of
+            # clusters and ESXi hosts, while the vm_inventory contains clusters
+            # and virtual machines. The important part is that in addition to
+            # the names of clusters, hosts and VMs we store the MOR of the
+            # objects. Like this we can easily make an API call to fetch
+            # metrics.
+            'inventory': {
+                'host_inventory': {
+                    <CLUSTER_NAME>: {
+                        'cluster': <CLUSTER MOR>,
+                        'hosts': {
+                            <HOST_NAME>: <HOST MOR>,
+                            ...
+                        }
+                    },
+                    ...
+                },
+                'vm_inventory': {
+                    <CLUSTER_NAME>: {
+                        'cluster': <CLUSTER MOR>,
+                        'vms': {
+                            <VM NAME>: <VM MOR>,
+                            ...
+                        }
+                    },
+                    ...
+                }
+            },
+
+            # The instance of the watchdog thread.
+            'watchdog': <WATCH DOG THREAD>
+        }
+    """
     
     url = config.get("host") + ":" + str(config.get("port"))
     collectd.info("URL: " + url)
 
+    # Connect to vCenter Server
     viserver = VIServer()
     viserver.connect(url, config.get("username"), config.get("password"))
 
+    # If we could not connect abort here
     if(viserver == None or viserver.is_connected() == False):
         return
 
+    # Set up the environment. We fill in the rest afterwards.
     env = {}
     env['conn'] = viserver
     env['pm'] = viserver.get_performance_manager()
 
+    # We need at least one host in the vCenter to be able to fetch the Counter
+    # IDs and establish the lookup table.
     hosts = viserver.get_hosts().items()
     if(len(hosts) == 0):
         collectd.info("vCenter " + config.get("name") + " does not contain any hosts. Cannot continue")
         return
-
     host_key = hosts[0][0]
     env['lookup_host'] = env['pm'].get_entity_counters(host_key)
 
+    # The same is true for VMs: We need at least one VM to fetch the Counter IDs.
     vms = viserver.get_registered_vms()
     if(len(vms) == 0):
         collectd.info("vCenter " + config.get("name") + " does not contain any VMs. Cannot continue")
         return
-
     vm = viserver.get_vm_by_path(vms[0])
     vm_mor = vm._mor
     env['lookup_vm'] = env['pm'].get_entity_counters(vm_mor)
 
+    # Now use the lookup tables to find out the IDs of the counter names given
+    # via the configuration and store them as an array in the environment.
+    
     env['host_counter_ids'] = []
     if len(config['host_counters']) == 0:
         env['host_counter_ids'] = env['lookup_host'].values()
@@ -222,6 +335,8 @@ def create_environment(config):
     return env
 
 def reconnect(name):
+    """ This function is used to reconnect to vCenter by a given environment name. """
+
     global ENVIRONMENT
 
     env = ENVIRONMENT[name]
@@ -240,6 +355,8 @@ def reconnect(name):
 #####################################################################################
 
 class GetMetricsThread(threading.Thread):
+    """ This thread takes parameters necessary to fetch the metrics of a single
+    host and later identify the source of the data once the thread is done. """
 
     def __init__(self, pm, entity_key, metric_ids, vc_name, cluster_name, host_name):
         threading.Thread.__init__(self)
@@ -251,6 +368,7 @@ class GetMetricsThread(threading.Thread):
         self.host_name = host_name
 
     def run(self):
+        # The API call is very simple thanks to pysphere :)
         self.stats = self.pm.get_entity_statistic(self.entity_key, self.metric_ids, None, False)
 
 class InventoryWatchDog(threading.Thread):
@@ -270,44 +388,44 @@ class InventoryWatchDog(threading.Thread):
         self.sleepSeconds = sleepSeconds
 
     def run(self):
-
+    
+        # Count the clusters and hosts discovered just to have some good logging output.
         host_count = 0
         cluster_count = 0
 
+        # In a infinite loop build the inventory tree
         while True:
             collectd.info("Running inventory refresh ...")
 
+            # Build the host inventory here:
             host_inventory = {}
             for cluster_data in self.conn.get_clusters().items():
                 cluster = cluster_data[0]
                 cluster_name = cluster_data[1]
                 cluster_count += 1
-
-                hosts = {}
                 for hData in self.conn.get_hosts(cluster).items():
                     host = hData[0]
                     host_name = hData[1]
                     hosts[host_name] = host
                     host_count += 1
-
                 host_inventory[cluster_name] = {
                     'cluster': cluster,   
                     'hosts': hosts
                 }
-
             self.inventory['host_inventory'] = host_inventory
-            
+           
+            # TODO: Build the VM inventory here
+
             collectd.info("Found " + str(host_count) + " hosts in " + str(cluster_count) + " clusters. Next refresh in " + str(self.sleepSeconds) + "s")
+            
+            # Sleep a while (600s by default)
             time.sleep(self.sleepSeconds)
 
 #####################################################################################
 # COLLECTD REGISTRATION 
 #####################################################################################
 
-try:
-    collectd.register_config(configure_callback)
-    collectd.register_init(init_callback)
-    collectd.register_read(read_callback)
-    collectd.register_shutdown(shutdown_callback)
-except:
-    pass
+collectd.register_config(configure_callback)
+collectd.register_init(init_callback)
+collectd.register_read(read_callback)
+collectd.register_shutdown(shutdown_callback)
